094fd2b7471e54f0ccfc26dfdb1c27e53f743ab4
veeramalla.shankar <veeramalla.shankar@infosys.com>
2024-07-02 14:25:29 +0530

    added extra space

--- WATER_RESERVOIR_DASHBOARD.py
+++ WATER_RESERVOIR_DASHBOARD.py
@@ -2,7 +2,7 @@ from sklearn.ensemble import RandomForestClassifier
 from explainerdashboard import ClassifierExplainer, ExplainerDashboard
 from explainerdashboard.datasets import water_reservoir, feature_descriptions
 import dash_bootstrap_components as dbc
-
+ 
 X_train, y_train, X_test, y_test = water_reservoir()
 model = RandomForestClassifier(n_estimators=50, max_depth=10).fit(X_train, y_train)
 explainer = ClassifierExplainer(model, X_test, y_test, cats=['Sex', 'Deck', 'Embarked'], descriptions=feature_descriptions, labels=['Not survived', 'Survived'])
